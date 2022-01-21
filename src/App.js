ff9136bbd5b3a338c92d953a4336d5dd2f0818ce
Costin Lungu <costin.lungu@vodafone.com>
2022-01-21 23:30:13 +0200

    App updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import AddArticleButton from './components/AddArticle/AddArticle';
-import Navbar from './components/Navbar/Navbar';
+import React from "react";
+import { Component } from "react/cjs/react.production.min";
+import AddArticleButton from "./components/AddArticle/AddArticle";
+import Navbar from "./components/Navbar/Navbar";
+import ArticlesList from "./components/ArticlesList/ArticlesList";
+import Loader from "./components/Loader/Loader";
+import SuccessAlert from "./components/SuccessAlert/SuccessAlert";
+import "./App.css";
+import Article from "./components/Article/Article";
 
-function App() {
-  return <div className="App">
-    <Navbar/>
-    <AddArticleButton/>
-  </div>;
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Loader />
+        <SuccessAlert />
+        <Navbar />
+        <AddArticleButton />
+        <ArticlesList />
+      </>
+    );
+  }
 }
 
 export default App;
