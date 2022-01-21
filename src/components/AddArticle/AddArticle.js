1c8bf32b9d86afa217b93883b43d60430224fc4b
Costin Lungu <costin.lungu@vodafone.com>
2022-01-21 23:25:45 +0200

    Added AddArticle - button component

--- src/components/AddArticle/AddArticle.js
+++ src/components/AddArticle/AddArticle.js
@@ -1,12 +1,11 @@
 import React from "react";
+import "./AddArticle.css";
 
 function AddArticle() {
   return (
     <div className="add-article-button">
       <div className="add__container">
-          <button className="button open-modal fas fa-plus">
-              Add Button
-          </button>
+        <button className="button open-modal fas fa-plus"> Add Button</button>
       </div>
     </div>
   );
