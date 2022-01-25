8889fbbd79984f370aa1d87ee86975f621183751
stefanbcs <stefan.bcs10@gmail.com>
2022-01-25 16:56:55 +0200

    added browser router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Details from "./routes/Details";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App /> 
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="details" element={<Details />}>
+          <Route path=":id" element={<Details />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
