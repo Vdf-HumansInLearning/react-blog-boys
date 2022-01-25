4c1dcbb194ea3557db870f71f30055c5151ffda5
bogdanmogos <bogdan.mogos@vodafone.com>
2022-01-25 16:21:31 +0200

    added missing classes

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -5,13 +5,13 @@ function Article({ article }) {
   return (
     <div id={article.id}>
       <h2 className="title">{article.title}</h2>
-      <ul>
+      <ul className="info__container">
         <li className="info__item">{article.tag}</li>
         <li className="info__item">
           Added by
-          <span className="info__mark point">{article.author}</span>
+          <span className="info__mark point"> {article.author}</span>
         </li>
-        <li className="info__item"></li>
+        <li className="info__item">{article.date}</li>
       </ul>
       <div className="actions__container">
         <button type="button" className="actions__btn border" id={article.id}>
