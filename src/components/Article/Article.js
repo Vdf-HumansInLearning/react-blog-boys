85e859e213f118a3a9cd095ffd5e5e11fd233e80
Costin Lungu <costin.lungu@vodafone.com>
2022-01-27 11:56:15 +0200

    Refactoring: function to class

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -1,39 +1,43 @@
 import React from "react";
+import { Component } from "react/cjs/react.production.min";
 import "./Article.css";
 
-function Article({ article }) {
-  return (
-    <div id={article.id}>
-      <h2 className="title">{article.title}</h2>
-      <ul className="info__container">
-        <li className="info__item">{article.tag}</li>
-        <li className="info__item">
-          Added by
-          <span className="info__mark point"> {article.author}</span>
-        </li>
-        <li className="info__item">{article.date}</li>
-      </ul>
-      <div className="actions__container">
-        <button type="button" className="actions__btn border" id={article.id}>
-          Edit
-        </button>
-        <button type="button" className="actions__btn" id={article.id}>
-          Delete
-        </button>
-      </div>
-      <img src={article.imgUrl} alt={article.imgAlt}></img>
-      <div className="content__container">
-        <p>{article.content}</p>
-      </div>
-      <div className="readmore__container">
-        <a className="btn-details" href={"#/article/" + article.id}>
-          <button type="button" className="button button-details">
-            Read More
+class Article extends Component {
+  render() {
+    const article = this.props.article;
+    return (
+      <div id={article.id}>
+        <h2 className="title">{article.title}</h2>
+        <ul className="info__container">
+          <li className="info__item">{article.tag}</li>
+          <li className="info__item">
+            Added by
+            <span className="info__mark point"> {article.author}</span>
+          </li>
+          <li className="info__item">{article.date}</li>
+        </ul>
+        <div className="actions__container">
+          <button type="button" className="actions__btn border" id={article.id}>
+            Edit
           </button>
-        </a>
+          <button type="button" className="actions__btn" id={article.id}>
+            Delete
+          </button>
+        </div>
+        <img src={article.imgUrl} alt={article.imgAlt}></img>
+        <div className="content__container">
+          <p>{article.content}</p>
+        </div>
+        <div className="readmore__container">
+          <a className="btn-details" href={"#/article/" + article.id}>
+            <button type="button" className="button button-details">
+              Read More
+            </button>
+          </a>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Article;
