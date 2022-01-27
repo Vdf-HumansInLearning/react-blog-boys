46678e13f5ed6bc0fbb851f6120e98e87623755f
Costin Lungu <costin.lungu@vodafone.com>
2022-01-27 11:58:57 +0200

    -Added functionality for button Add Article; -And moved modal component in Add Article component

--- src/components/AddArticle/AddArticle.js
+++ src/components/AddArticle/AddArticle.js
@@ -1,14 +1,30 @@
 import React from "react";
+import { Component } from "react/cjs/react.production.min";
+import ModalAddArticle from "../ModalAddArticle/ModalAddArticle";
 import "./AddArticle.css";
 
-function AddArticle() {
-  return (
-    <div className="add-article-button">
-      <div className="add__container">
-        <button className="button open-modal fas fa-plus"> Add Article</button>
+class AddArticle extends Component {
+  constructor(props) {
+    super(props);
+    props = this.props;
+  }
+  render() {
+    return (
+      <div className="add-article-button">
+        <div className="add__container">
+          <button
+            className="button open-modal fas fa-plus"
+            onClick={this.props.openModal}
+          >
+            Add Article
+          </button>
+          <ModalAddArticle
+            showModal={this.props.showModal}
+            closeModal={this.props.closeModal}
+          />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-
 export default AddArticle;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,25 +1,45 @@
 import React from "react";
 import { Component } from "react/cjs/react.production.min";
-import AddArticleButton from "./../components/AddArticle/AddArticle";
+import AddArticle from "./../components/AddArticle/AddArticle";
 import Navbar from "./../components/Navbar/Navbar";
 import ArticlesList from "./../components/ArticlesList/ArticlesList";
 import Loader from "./../components/Loader/Loader";
 import SuccessAlert from "./../components/SuccessAlert/SuccessAlert";
 import "./../Home.css";
 import FooterLinks from "./../components/FooterLinks/FooterLinks";
-import ModalAddArticle from "./../components/ModalAddArticle/ModalAddArticle";
 import ModalAlert from "./../components/ModalAlert/ModalAlert";
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showModal: false,
+    };
+    this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+
+  openModal() {
+    this.setState({ showModal: true });
+  }
+
+  closeModal() {
+    this.setState({ showModal: false });
+  }
+
   render() {
     return (
       <>
         <Loader />
         <SuccessAlert />
         <Navbar />
-        <ModalAddArticle />
+
         <ModalAlert />
-        <AddArticleButton />
+        <AddArticle
+          showModal={this.state.showModal}
+          openModal={this.openModal}
+          closeModal={this.closeModal}
+        />
         <ArticlesList />
         <FooterLinks />
       </>
