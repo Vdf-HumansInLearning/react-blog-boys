86fec31eb5c0b7ee95430a1a43c11bb66eb4282d
stefanbcs <stefan.bcs10@gmail.com>
2022-01-27 19:03:34 +0200

    added ArticleDetails component

---
+++ src/components/ArticleDetails/ArticleDetails.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Component } from "react/cjs/react.production.min";
+import "./../Article/Article.css";
+
+class ArticleDetails extends Component {
+  render() {
+    const article = this.props.article;
+    return (
+      <>
+        <div id={article.id}>
+          <h2 className="title">{article.title}</h2>
+          <ul className="info__container">
+            <li className="info__item">{article.tag}</li>
+            <li className="info__item">
+              Added by
+              <span className="info__mark point"> {article.author}</span>
+            </li>
+            <li className="info__item">{article.date}</li>
+          </ul>
+          <img src={article.imgUrl} alt={article.imgAlt}></img>
+          <div className="content__container">
+            <p>{article.content}</p>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+
+export default ArticleDetails;
