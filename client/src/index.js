3139638fbd9a4d6ae839de35a6a4edce4cef2c33
bogdanmogos <bogdan.mogos@vodafone.com>
2022-01-31 16:29:03 +0200

    Added 404 page and routes

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import Details from "./routes/Details";
+import Page404 from "./routes/Page404";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -13,6 +14,8 @@ ReactDOM.render(
         <Route path="details" element={<Details />}>
           <Route path=":id" element={<Details />} />
         </Route>
+        <Route path="not-found" element={<Page404 />} />
+        <Route path="*" element={<Page404 />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
---
+++ client/src/routes/Page404.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Error404 from "../components/Error404/Error404";
+import Navbar from "./../components/Navbar/Navbar";
+import "./../Home.css";
+
+
+
+
+function Page404() {
+  
+    return (
+      <>
+      <Navbar />
+      <Error404 />
+      
+      </>
+    );
+  }
+  
+  export default Page404;
