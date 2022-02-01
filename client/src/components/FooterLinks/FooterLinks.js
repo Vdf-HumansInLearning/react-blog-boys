2d6e4ad277d74f120a3b75f29744f791d7f343ae
stefanbcs <stefan.bcs10@gmail.com>
2022-02-01 20:34:33 +0200

    updated footer links functionality

--- client/src/components/FooterLinks/FooterLinks.js
+++ client/src/components/FooterLinks/FooterLinks.js
@@ -1,14 +1,46 @@
 import React from "react";
 import "./FooterLinks.css";
 
-
-function FooterLinks() {
+function FooterLinks(props) {
+  if (props.route === "home") {
     return (
-        <footer className="footer next-button-class">
-            <button className="footer__link footer__link--previous" id="button-prev">previous</button>
-            <button className="footer__link footer__link--next" id="button-next">next</button>
-        </footer >
+      <footer className="footer next-button-class">
+        <button
+          className="footer__link footer__link--previous"
+          id="button-prev"
+          onClick={props.handlePrevious}
+        >
+          previous
+        </button>
+        <button
+          className="footer__link footer__link--next"
+          id="button-next"
+          onClick={props.handleNext}
+        >
+          next
+        </button>
+      </footer>
     );
+  }
+  if (props.route === "details") {
+    return (
+      <footer className="footer next-button-class">
+        <a className="anchor__link" href={props.previousArticle}>
+          <button
+            className="footer__link footer__link--previous"
+            id="button-prev"
+          >
+            previous article
+          </button>
+        </a>
+        <a className="anchor__link" href={props.nextArticle}>
+          <button className="footer__link footer__link--next" id="button-next">
+            next article
+          </button>
+        </a>
+      </footer>
+    );
+  }
 }
 
 export default FooterLinks;
