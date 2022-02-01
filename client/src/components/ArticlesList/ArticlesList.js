a01b4730ea5a582d1fe1728bf9e9ceb9522bd39a
Costin Lungu <costin.lungu@vodafone.com>
2022-02-01 12:58:53 +0200

    Deleted unused class

--- client/src/components/ArticlesList/ArticlesList.js
+++ client/src/components/ArticlesList/ArticlesList.js
@@ -1,62 +1,61 @@
-import React from "react";
-import { Component } from "react/cjs/react.production.min";
-import Article from "../Article/Article";
-import "./ArticlesList.css";
+// import React from "react";
+// import { Component } from "react/cjs/react.production.min";
+// import Article from "../Article/Article";
+// import "./ArticlesList.css";
 
-class ArticlesList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      articlesList: [],
-    };
-    this.renderArticles = this.renderArticles.bind(this);
-  }
+// class ArticlesList extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       articlesList: [],
+//     };
+//     this.renderArticles = this.renderArticles.bind(this);
+//   }
 
-  renderArticles() {
-    const self = this;
-    fetch("http://localhost:3007/articles")
-      .then(function (response) {
-        if (response.status !== 200) {
-          console.log(
-            "looks like there was a problem. Status Code " + response.status
-          );
-          return;
-        }
+//   renderArticles() {
+//     const self = this;
+//     fetch("http://localhost:3007/articles")
+//       .then(function (response) {
+//         if (response.status !== 200) {
+//           console.log(
+//             "looks like there was a problem. Status Code " + response.status
+//           );
+//           return;
+//         }
 
-        response.json().then(function (data) {
-          self.setState({
-            articlesList: data.articles,
-          });
-        });
-      })
-      .catch(function (err) {
-        console.log("Fetch Error :-S", err);
-      });
-  }
+//         response.json().then(function (data) {
+//           self.setState({
+//             articlesList: data.articles,
+//           });
+//         });
+//       })
+//       .catch(function (err) {
+//         console.log("Fetch Error :-S", err);
+//       });
+//   }
 
-  componentDidMount() {
-    this.renderArticles();
-  }
+//   componentDidMount() {
+//     this.renderArticles();
+//   }
 
-  render() {
-    const { articlesList } = this.state;
-    console.log(articlesList);
-    const articles = articlesList.map((article) => (
-      <Article
-        article={article}
-        key={article.id}
-        showModalAlert={this.props.showModalAlert}
-        openModal={this.props.openModal}
-        closeModal={this.props.closeModal}
-      />
-    ));
+//   render() {
+//     const { articlesList } = this.state;
+//     const articles = articlesList.map((article) => (
+//       <Article
+//         article={article}
+//         key={article.id}
+//         showModalAlert={this.props.showModalAlert}
+//         openModal={this.props.openModal}
+//         closeModal={this.props.closeModal}
+//       />
+//     ));
 
-    return (
-      <div id="root-articlesList" className="main error">
-        <article>{articles}</article>
-      </div>
-    );
-  }
-}
+//     return (
+//       <div id="root-articlesList" className="main error">
+//         <article>{articles}</article>
+//       </div>
+//     );
+//   }
+// }
 
-export default ArticlesList;
+// export default ArticlesList;
