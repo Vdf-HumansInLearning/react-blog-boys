50a866d6e9918a7318ee890fddd78d1fef555b12
stefanbcs <stefan.bcs10@gmail.com>
2022-02-02 11:07:33 +0200

    remove prev/next buttons when needed

--- client/src/components/FooterLinks/FooterLinks.js
+++ client/src/components/FooterLinks/FooterLinks.js
@@ -5,39 +5,58 @@ function FooterLinks(props) {
   if (props.route === "home") {
     return (
       <footer className="footer next-button-class">
-        <button
-          className="footer__link footer__link--previous"
-          id="button-prev"
-          onClick={props.handlePrevious}
-        >
-          previous
-        </button>
-        <button
-          className="footer__link footer__link--next"
-          id="button-next"
-          onClick={props.handleNext}
-        >
-          next
-        </button>
+        {props.indexStart !== 0 ? (
+          <button
+            className="footer__link footer__link--previous"
+            id="button-prev"
+            onClick={props.handlePrevious}
+          >
+            previous
+          </button>
+        ) : (
+          <span></span>
+        )}
+        {props.indexEnd < props.totalNumberOfArticles - 1 ? (
+          <button
+            className="footer__link footer__link--next"
+            id="button-next"
+            onClick={props.handleNext}
+          >
+            next
+          </button>
+        ) : (
+          <span></span>
+        )}
       </footer>
     );
   }
   if (props.route === "details") {
     return (
       <footer className="footer next-button-class">
-        <a className="anchor__link" href={props.previousArticle}>
-          <button
-            className="footer__link footer__link--previous"
-            id="button-prev"
-          >
-            previous article
-          </button>
-        </a>
-        <a className="anchor__link" href={props.nextArticle}>
-          <button className="footer__link footer__link--next" id="button-next">
-            next article
-          </button>
-        </a>
+        {props.previousArticle ? (
+          <a className="anchor__link" href={props.previousArticle}>
+            <button
+              className="footer__link footer__link--previous"
+              id="button-prev"
+            >
+              previous article
+            </button>
+          </a>
+        ) : (
+          <span></span>
+        )}
+        {props.nextArticle ? (
+          <a className="anchor__link" href={props.nextArticle}>
+            <button
+              className="footer__link footer__link--next"
+              id="button-next"
+            >
+              next article
+            </button>
+          </a>
+        ) : (
+          <span></span>
+        )}
       </footer>
     );
   }
